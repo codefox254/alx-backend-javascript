d05548d88744acd5250d9f54ba6a2e379138db91
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-07-26 23:58:50 +0300

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,3 @@
-// 0-constants.js
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -14,4 +13,3 @@ export function taskNext() {
 
   return combination;
 }
-
