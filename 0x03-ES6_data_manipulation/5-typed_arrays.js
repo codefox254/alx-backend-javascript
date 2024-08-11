464d1b337fc0bb97697dbb9ca68f45df8f8d2d64
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-08-11 22:20:21 +0300

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,13 @@
 function createInt8TypedArray(length, position, value) {
-    const buffer = new ArrayBuffer(length);
-    const int8Array = new Int8Array(buffer);
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
 
-    if (position >= length || position < 0) {
-        throw new Error('Position outside range');
-    }
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
 
-    int8Array[position] = value;
-    return buffer;
+  int8Array[position] = value;
+  return buffer;
 }
 
 export default createInt8TypedArray;
-
