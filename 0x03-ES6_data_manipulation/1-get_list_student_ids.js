48bc2a2c90698a6b7db11fbf8aaf86a7d07872c9
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-08-11 21:53:27 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,8 @@
-// 1-get_list_student_ids.js
 function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    return students.map(student => student.id);
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id); // Added parentheses around arrow function argument
 }
 
 export default getListStudentIds;
