753bf6a6f178e0866ffc598254239ad83992b01a
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-10-19 00:03:24 -0700

    Update 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,7 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+function calculateNumber(a, b) {
+  const roundedA = Math.round(a);  // Round the first number
+  const roundedB = Math.round(b);  // Round the second number
+  return roundedA + roundedB;      // Return the sum of rounded numbers
+}
 
 module.exports = calculateNumber;
