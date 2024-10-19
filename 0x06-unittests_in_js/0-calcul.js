b5c7f28fbc8933a9f51c0102c72667a0e39ab0ce
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-10-19 00:15:26 -0700

    Update 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,8 @@
-module.exports = function calculateNumber(a, b = 0) {
-  const aNum = Number(a);
-  const bNum = Number(b);
+function calculateNumber(a, b) {
+    const an = Math.round(a);
+    const bn = Math.round(b);
+    const c = an + bn;
+    return c;
+}
 
-  if (Number.isNaN(aNum) || Number.isNaN(bNum))
-    throw TypeError('Parameters must be numbers');
-
-  return Math.round(aNum) + Math.round(bNum);
-};
+module.exports = calculateNumber;
